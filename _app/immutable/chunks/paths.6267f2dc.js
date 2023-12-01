d00790f70032b9a8e504ebb66422a4f59b47fde9
Tristan Cragnolini <tristan.cragnolini@gmail.com>
2023-12-01 17:33:13 +0000

    Updates

---
+++ _app/immutable/chunks/paths.6267f2dc.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_8y5ry4)==null?void 0:s.base)??"/cragnolini-lab";var a;const t=((a=globalThis.__sveltekit_8y5ry4)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ _app/immutable/nodes/3.e8788e37.js
@@ -0,0 +1,4 @@
+import{s,n as a}from"../chunks/scheduler.e108d1fd.js";import{S as o,i as r,g as m,h,x as u,a as c,f}from"../chunks/index.4d21fe7b.js";function p(n){let e,i=`<h1>Members of the lab</h1> <ul><li>Gemma White</li> <li>Alison Faid</li> <li>Arek Wylegalski</li> <li>Anuregah Erlaut</li></ul> <h1>Previous Members</h1> <ul><li>Neil Anthony Magno</li></ul>
+
+
+Computational biophysics at <a href="https://www.bbk.ac.uk/">Birkbeck, University of London.</a>`;return{c(){e=m("main"),e.innerHTML=i},l(t){e=h(t,"MAIN",{"data-svelte-h":!0}),u(e)!=="svelte-1ytaig0"&&(e.innerHTML=i)},m(t,l){c(t,e,l)},p:a,i:a,o:a,d(t){t&&f(e)}}}class b extends o{constructor(e){super(),r(this,e,null,p,s,{})}}export{b as component};
