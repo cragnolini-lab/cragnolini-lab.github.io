a332ca23118e990d5b42a2251c46107e65201c9e
Tristan Cragnolini <tristan.cragnolini@gmail.com>
2023-12-06 16:53:40 +0000

    Add a Tile component, and add a few default ones on the project page

---
+++ src/routes/Tile.svelte
@@ -0,0 +1,26 @@
+<section class="tile">
+	<header>
+		<h1>
+			<slot name="title">
+				Title
+			</slot>
+		</h1>
+	</header>
+	<slot name="summary">
+		A short summary of this tile.
+	</slot>
+	<footer>
+		<slot name="link">
+			See more at...
+		</slot>
+	</footer>
+</section>
+
+<style>
+.tile {
+	//display: flex;
+	background-color: rgba(0, 0, 0, 0.1);
+	padding: 32px;
+	border-radius: 8px;
+}
+</style>
--- src/routes/research/+page.svelte
+++ src/routes/research/+page.svelte
@@ -1,5 +1,24 @@
+<script>
+	import Tile from '../Tile.svelte';
+</script>
+
 <main>
 <h1>Our research</h1>
 
+<div class="projects">
+<Tile />
+<Tile />
+<Tile />
+<Tile />
+</div>
+
 Computational biophysics at <a href="https://www.bbk.ac.uk/">Birkbeck, University of London.</a>
 </main>
+
+<style>
+.projects {
+	display: grid;
+	grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+	gap: 32px;
+}
+</style>
