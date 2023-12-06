2d922a0771b93abc8e006159410968290145c7ed
Tristan Cragnolini <tristan.cragnolini@gmail.com>
2023-12-06 17:57:49 +0000

    Updates

---
+++ _app/immutable/chunks/github.8246e764.js
@@ -0,0 +1,1 @@
+const e=""+new URL("../assets/github.1ea8d62e.svg",import.meta.url).href;export{e as g};
--- _app/immutable/nodes/4.6ddd23c2.js
+++
@@ -1,3 +0,0 @@
-import{s as i,n as a}from"../chunks/scheduler.e108d1fd.js";import{S as r,i as l,g as m,h as c,x as f,a as h,f as p}from"../chunks/index.4d21fe7b.js";function u(s){let e,n=`<h1>Our research</h1>
-
-Computational biophysics at <a href="https://www.bbk.ac.uk/">Birkbeck, University of London.</a>`;return{c(){e=m("main"),e.innerHTML=n},l(t){e=c(t,"MAIN",{"data-svelte-h":!0}),f(e)!=="svelte-fgma9v"&&(e.innerHTML=n)},m(t,o){h(t,e,o)},p:a,i:a,o:a,d(t){t&&p(e)}}}class v extends r{constructor(e){super(),l(this,e,null,u,i,{})}}export{v as component};
