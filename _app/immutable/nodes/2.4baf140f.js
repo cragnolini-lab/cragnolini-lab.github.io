c02daebbd20e44df3c663aeb3104fd33f75e79d9
Tristan Cragnolini <tristan.cragnolini@gmail.com>
2023-12-07 20:24:49 +0000

    Updates

--- _app/immutable/nodes/2.4baf140f.js
+++
@@ -1,3 +0,0 @@
-import{s as o,n as a}from"../chunks/scheduler.63274e7e.js";import{S as r,i as l,g as m,h as c,x as p,a as f,f as h}from"../chunks/index.d32a183c.js";function u(s){let t,n=`<h1>Cragnolini lab</h1>
-
-Computational biophysics at <a href="https://www.bbk.ac.uk/">Birkbeck, University of London.</a>`;return{c(){t=m("main"),t.innerHTML=n},l(e){t=c(e,"MAIN",{"data-svelte-h":!0}),p(t)!=="svelte-1dx89lj"&&(t.innerHTML=n)},m(e,i){f(e,t,i)},p:a,i:a,o:a,d(e){e&&h(t)}}}class x extends r{constructor(t){super(),l(this,t,null,u,o,{})}}export{x as component};
