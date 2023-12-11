5c3de0355aaec82e23cfe2c1d94361af1a55b45d
Tristan Cragnolini <tristan.cragnolini@gmail.com>
2023-12-11 20:42:40 +0000

    Updates

--- _app/immutable/nodes/2.d66d318a.js
+++
@@ -1,6 +0,0 @@
-import{s as i,n as a}from"../chunks/scheduler.e108d1fd.js";import{S as r,i as l,g as c,h as m,x as d,a as u,f}from"../chunks/index.ed8b9549.js";function h(s){let e,n=`<h1>Cragnolini lab</h1>
-
-
-Computational biophysics lab at <a href="https://www.bbk.ac.uk/">Birkbeck, University of London.</a> <br/>
-
-We do research using molecular modelling, statistical physics, and inference methods to understand the structure and dynamics of biomolecular complexes.`;return{c(){e=c("main"),e.innerHTML=n},l(t){e=m(t,"MAIN",{"data-svelte-h":!0}),d(e)!=="svelte-10ofenf"&&(e.innerHTML=n)},m(t,o){u(t,e,o)},p:a,i:a,o:a,d(t){t&&f(e)}}}class g extends r{constructor(e){super(),l(this,e,null,h,i,{})}}export{g as component};
