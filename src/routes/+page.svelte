32e856f4f55d96ec5799c6b616bf2ae83ecfd4ea
Tristan Cragnolini <tristan.cragnolini@gmail.com>
2023-12-11 20:42:20 +0000

    Improved landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,29 @@
 <main>
 <h1>Cragnolini lab</h1>
 
-
-Computational biophysics lab at <a href="https://www.bbk.ac.uk/">Birkbeck, University of London.</a>
+<h2>
+	Computational biophysics lab at <a href="https://www.bbk.ac.uk/">Birkbeck, University of London.</a>
+</h2>
 
 <br />
 
-We do research using molecular modelling, statistical physics, and inference methods to understand the structure and dynamics of biomolecular complexes.
+<p>
+	Our work combines molecular modelling, statistical physics, and inference methods to understand the structure and dynamics of biomolecular complexes.
+</p>
+
+<p>
+	A central question we are trying to answer is how the presence of spatial and temporal ensembles can be represented theoretically, and incorporated in the interpretation of experimental data..
+</p>
+
+<p>
+	We harness structural biology data to confront our models to experiment, and use predictive deep learning model to inform our interpretation of this experimental data.
+</p>
 
 </main>
+
+<style>
+main {
+	max-width: 1000px;
+	font-size: 1.2em;
+}
+</style>
